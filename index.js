9a8fda9b92db4b93e5665db309040f9c44f5e53e
Robert-ArchLinux <robertsolorzano510@gmail.com>
2024-03-20 15:21:10 -0700

    Created index.js and started outline
    -imported inquirer and connection.js
    -created displayMenu() to handle inquirer questions and answers
    -implemented all choices based of instructions/user story
    -created functions (names) for each case in the user choice, will need to
    implement logic for each
    -script runs by calling displayMenu()
    
    -installed console.table npm package for later use, will use for clean tables in the
    command line enhacing user experience

---
+++ index.js
@@ -0,0 +1,56 @@
+const inquirer = require('inquirer');
+const connection = require('./config/connection');
+
+// Function to display the main menu
+function displayMenu() {
+  inquirer
+    .prompt({
+      type: 'list',
+      name: 'choice',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit',
+      ],
+      loop: false,
+    })
+    .then((answer) => {
+      switch (answer.choice) {
+        case 'View all departments':
+          viewAllDepartments();//need to make later
+          break;
+        case 'View all roles':
+          viewAllRoles();//need to make later
+          break;
+        case 'View all employees':
+          viewAllEmployees();//need to make later
+          break;
+        case 'Add a department':
+          addDepartment();//need to make later
+          break;
+        case 'Add a role':
+          addRole();//need to make later
+          break;
+        case 'Add an employee':
+          addEmployee();//need to make later
+          break;
+        case 'Update an employee role':
+          updateEmployeeRole();//need to make later
+          break;
+        case 'Exit':
+          connection.end();
+          console.log('Goodbye!');
+          break;
+      }
+    });
+}
+
+
+// Start the application
+displayMenu();
