613ee1cba477fdfe12a8b3d97edf1532df4f234e
Robert-ArchLinux <robertsolorzano510@gmail.com>
2024-03-21 11:45:27 -0700

    Created viewDepartmentBudget()
    -Tackled another bonus option to add all budgets by department
    -This will help the user keep track of all finacials per dept. making it
    easier for the user
    -Utilizes the SUM function in SQL to totalize the deptament budget

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function displayMenu() {
         'View all departments',
         'View all roles',
         'View all employees',
+        'View department budgets',
         'Add a department',
         'Add a role',
         'Add an employee',
@@ -31,6 +32,9 @@ function displayMenu() {
           break;
         case 'View all employees':
           viewAllEmployees();
+          break;
+        case 'View department budgets':
+          viewDepartmentBudget();
           break;
         case 'Add a department':
           addDepartment();
@@ -324,6 +328,25 @@ function updateEmployeeManager() {
           }
         );
       });
+  });
+}
+
+// Function to view the total utilized budget of a department
+function viewDepartmentBudget() {
+  const query = `
+    SELECT 
+      department.name AS department,
+      SUM(role.salary) AS total_budget
+    FROM employee
+    JOIN role ON employee.role_id = role.id
+    JOIN department ON role.department_id = department.id
+    GROUP BY department.name
+  `;
+
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    displayMenu();
   });
 }
 
