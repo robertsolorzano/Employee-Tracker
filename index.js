65faa713f53f7ff8e53494111e6eecc6a2abe01a
Robert-ArchLinux <robertsolorzano510@gmail.com>
2024-03-21 12:07:37 -0700

    Created budgets.js
    -Moved view department budget function into its own file
    -Exported as module

--- index.js
+++ index.js
@@ -331,24 +331,24 @@ function displayMenu() {
 //   });
 // }
 
-// Function to view the total utilized budget of a department
-function viewDepartmentBudget() {
-  const query = `
-    SELECT 
-      department.name AS department,
-      SUM(role.salary) AS total_budget
-    FROM employee
-    JOIN role ON employee.role_id = role.id
-    JOIN department ON role.department_id = department.id
-    GROUP BY department.name
-  `;
+// // Function to view the total utilized budget of a department
+// function viewDepartmentBudget() {
+//   const query = `
+//     SELECT 
+//       department.name AS department,
+//       SUM(role.salary) AS total_budget
+//     FROM employee
+//     JOIN role ON employee.role_id = role.id
+//     JOIN department ON role.department_id = department.id
+//     GROUP BY department.name
+//   `;
 
-  connection.query(query, (err, res) => {
-    if (err) throw err;
-    console.table(res);
-    displayMenu();
-  });
-}
+//   connection.query(query, (err, res) => {
+//     if (err) throw err;
+//     console.table(res);
+//     displayMenu();
+//   });
+// }
 
 // Start the application
 displayMenu();
